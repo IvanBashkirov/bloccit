74b5e7819cd586cf833147a7a352a013d6f9e9a5
Ivan Bashkirov <ivanbashkirov@protonmail.com>
2017-03-19 18:36:29 +0000

    rails assignment 18 completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,9 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @posts.each_with_index do |post, ind|
+      post.title = "SPAM" if ind%5 == 0
+    end
   end
 
   def show
