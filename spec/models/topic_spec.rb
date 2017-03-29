e4b434c5c4ad51f21034f3898477339d5cca5cfd
Ivan Bashkirov <ivanbashkirov@protonmail.com>
2017-03-29 01:09:32 +0100

    rails assignment 23 completed

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -4,9 +4,14 @@ RSpec.describe Topic, type: :model do
   let(:name) { RandomData.random_sentence }
   let(:description) { RandomData.random_paragraph }
   let(:public) { true }
-  let(:topic) { Topic.create!(name: name, description: description) }
+  let(:topic)  { Topic.create!(name: name, description: description) }
 
   it { is_expected.to have_many(:posts) }
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_presence_of(:description) }
+
+  it { is_expected.to validate_length_of(:name).is_at_least(5) }
+  it { is_expected.to validate_length_of(:description).is_at_least(15) }
 
   describe 'attributes' do
     it 'has name, description, and public attributes' do
@@ -18,19 +23,5 @@ RSpec.describe Topic, type: :model do
     end
   end
 
-  describe 'POST create' do
-  it 'increases the number of topics by 1' do
-    expect { post :create, topic: { name: RandomData.random_sentence, description: RandomData.random_paragraph } }}.to change(Topic, :count).by(1)
-  end
 
-  it 'assigns Topic.last to @topic' do
-    post :create, topic: { name: RandomData.random_sentence, description: RandomData.random_paragraph }
-    expect(assigns(:topic)).to eq Topic.last
-  end
-
-  it 'redirects to the new topic' do
-    post :create, topic: { name: RandomData.random_sentence, description: RandomData.random_paragraph }
-    expect(response).to redirect_to Topic.last
-  end
-  end
 end
