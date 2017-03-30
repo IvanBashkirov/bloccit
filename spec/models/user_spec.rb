02d26eefba4d8e9b4615f85631161cb3db48983b
Ivan Bashkirov <ivanbashkirov@protonmail.com>
2017-03-30 16:26:58 +0100

    rails assignment 24 completed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,16 +20,21 @@ RSpec.describe User, type: :model do
     end
   end
 
+  describe 'user name' do
+    it 'should capitalize every word of the name' do
+      new_user = User.create!(name: 'rob jonson', email: 'rob@bloccit.com', password: '123456')
+      new_user2 = User.create!(name: 'rob jonson', email: 'rob2@bloccit.com', password: '123456')
+      expect(new_user).to have_attributes(name: 'Rob Jonson', email: 'rob@bloccit.com', password: '123456')
+      expect(new_user2).to have_attributes(name: 'Rob Jonson', email: 'rob2@bloccit.com', password: '123456')
+    end
+  end
+
   describe 'invalid user' do
     let(:user_with_invalid_name) { User.new(name: '', email: 'user@bloccit.com') }
     let(:user_with_invalid_email) { User.new(name: 'Bloccit User', email: '') }
 
     it 'should be an invalid user due to blank name' do
       expect(user_with_invalid_name).to_not be_valid
-    end
-
-    it 'should be an invalid user due to blank email' do
-      expect(user_with_invalid_email).to_not be_valid
     end
   end
 end
