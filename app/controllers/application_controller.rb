f7d4ccd829f6e8b191499064b168d32e419ef6d2
Ivan Bashkirov <ivanbashkirov@protonmail.com>
2017-04-06 03:04:41 +0100

    sign up confirmation deleted

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception, prepend: true
+  protect_from_forgery with: :exception
   include SessionsHelper
 
   private
