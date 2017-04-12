bcd7f576577f2d462f1184450a4ae02cd08743b9
Ivan Bashkirov <ivanbashkirov@protonmail.com>
2017-04-12 15:11:22 +0100

    favorite_posts are now accessible from user :through favorites

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :favorite_posts, through: :favorites, source: :post 
 
   before_save -> { self.email = email.downcase if email.present? },
               -> { self.name = capitalize_name(name) if name.present? },
